72b13694ad7aa721baf14481f863e32036b3ca56
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2020-10-07 09:19:31 -0700

    Update pipx.completions.bash

--- pipx.completions.bash
+++ pipx.completions.bash
@@ -1,5 +1,5 @@
 cite about-completion
-about-completion 'Install and Run Python Applications in Isolated Environments'
+about-completion 'install and run python applications in isolated environments'
 
 # Activate pipx completions
 eval "$(register-python-argcomplete pipx)"
